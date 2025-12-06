f5cfb3874d8cf627b478683240682fc0082d0894
leesugil <sugilmath@gmail.com>
2025-12-06 15:33:10 -0500

    removed interval submodule

--- src/masked_split/masked_split.py
+++ src/masked_split/masked_split.py
@@ -1,5 +1,5 @@
 import re
-from interval.interval import contains, union, excluding, is_intersecting, remove_overlapping_intervals, complements
+from interval.src.intervalop.interval import contains, union, excluding, is_intersecting, remove_overlapping_intervals, complements
 
 def get_delim_locations(s, delims):
     """
